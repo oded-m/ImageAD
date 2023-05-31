ffa53568ce622589bdb7d7d93ca66b864c774e59
USER001 <jhiu>
2023-05-31 08:26:52 +0300

    added paths to new dirctories

--- format_database.py
+++ format_database.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import os
+
+WORK_DIR = os.path.dirname(__file__)
+FM_DIR = 'feature_maps'
+BENCHMARK_DIR = os.path.join(WORK_DIR, 'benchmark')
+DATA_DIR = os.path.join(WORK_DIR, 'data')
+OUTPUTS_DIR = os.path.join(WORK_DIR, 'outputs')
 
 if __name__ == '__main__':
-    df = pd.read_csv('Image_Benchmark.csv')
+    path = os.path.join(OUTPUTS_DIR, 'Image_Benchmark.csv')
+    df = pd.read_csv(path)
     df = df[df['DatasetOk'] == 1]
     col_names = ['DatasetName', 'DatasetSource', 'DataType', 'PartitonMethod',
                  'AnomalyClassNames', 'TrainSize', 'TestSize',
@@ -13,5 +21,6 @@ if __name__ == '__main__':
         names.append('ILSVRC.' + str(i))
     df_new['DatasetName'] = names
     df_new.reset_index(drop=True, inplace=True)
-    df_new.to_csv('Image_Benchmark_Table.csv')
+    path = os.path.join(OUTPUTS_DIR, 'Image_Benchmark_Table.csv')
+    df_new.to_csv(path)
     print('Finnish')
